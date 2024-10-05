b0fe8b31918bb25415c7b55e6a9b4ca9b7783e4d
Ishika Chourase <ishikachourase4@gmail.com>
2024-10-05 16:22:54 +0530

    Structured Listings

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const ejsMate=require("ejs-mate");
 const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
 const {listingSchema, reviewSchema}=require("./schema.js");
+const listings= require("./routes/listing.js");
 app.use(methodoverride("_method"));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
@@ -17,6 +18,7 @@ app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 app.engine('ejs',ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
+
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/Wonderlust";
 
@@ -41,16 +43,7 @@ app.get("/",(req,res)=>{
 })
 
 
-const validateListing=(req,res,next)=>{
-  let {error}=listingSchema.validate(req.body);
 
-  if(error){
-    let errMsg=error.details.map((el)=>el.message).join(",");
-    throw new ExpressError(404,errMsg);
-  }else{
-    next();
-  }
-}
 
 const validateReview=(req,res,next)=>{
   let {error}=reviewSchema.validate(req.body);
@@ -62,63 +55,9 @@ const validateReview=(req,res,next)=>{
     next();
   }
 }
- // Index Route
-
- app.get("/listings",wrapAsync(async(req,res)=>{
-   const allListings= await Listing.find({});
-    res.render("listings/index.ejs",{allListings})
-    }));
-
-//New route
-app.get("/listings/new",(req,res)=>{
-    res.render("./listings/new.ejs");
- });
- 
 
 
-// Show Route
-app.get ("/listings/:id",wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-   const  listing = await Listing.findById(id).populate("reviews");
-    res.render("./listings/show.ejs",{listing});
-}))
-
-//Create Route
-
-app.post("/listings",validateListing,wrapAsync (async(req,res,next)=>{
-
-        const newListing = new Listing (req.body.listing)  //instance create kr rhe h
-        await newListing.save();
-        res.redirect("/listings");
-    
-   
-//let {title,description,image,price,country, location}=req.body;
-//let listing= req.body.listing;
-
-}))
-
-//Edit Route
-app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    const  listing = await Listing.findById(id);
-     res.render("./listings/edit.ejs",{listing});
-}))
-
-//Update Route
-app.put("/listings/:id",validateListing,wrapAsync(async(req,res)=>{
-
-    let {id}=req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});//js ki obj hai jiske ander sare parameters h recontruct krk unn parameter ko individual value me convert krenge jisko hum nayi updated value me pass krenge
-    res.redirect(`/listings/${id}`);
-}))
-
-//DELETE ROUTE
-app.delete("/listings/:id",wrapAsync( async(req,res)=>{
-    let {id}=req.params;
-    let deletedListing=await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-}))
+app.use("/listings",listings);
 
 //Reviews
 //Post Rewiew  Route
--- routes/listing.js
+++ routes/listing.js
@@ -1,2 +1,79 @@
 const express = require("express");
 const router= express.Router();
+const ExpressError=require("../utils/ExpressError.js");
+const wrapAsync=require("../utils/wrapAsync.js");
+const Listing = require("../models/listing.js");
+
+const {listingSchema, reviewSchema}=require("../schema.js");
+
+
+const validateListing=(req,res,next)=>{
+    let {error}=listingSchema.validate(req.body);
+  
+    if(error){
+      let errMsg=error.details.map((el)=>el.message).join(",");
+      throw new ExpressError(404,errMsg);
+    }else{
+      next();
+    }
+  }
+ // Index Route
+
+ router.get("/",wrapAsync(async(req,res)=>{
+    const allListings= await Listing.find({});
+     res.render("listings/index.ejs",{allListings})
+     }));
+ 
+ //New route
+ router.get("/new",(req,res)=>{
+     res.render("./listings/new.ejs");
+  });
+  
+ 
+ 
+ // Show Route
+ router.get ("/:id",wrapAsync(async(req,res)=>{
+     let {id}=req.params;
+    const  listing = await Listing.findById(id).populate("reviews");
+     res.render("./listings/show.ejs",{listing});
+ }))
+
+ 
+//Create Route
+
+router.post("/",validateListing,wrapAsync (async(req,res,next)=>{
+
+    const newListing = new Listing (req.body.listing)  //instance create kr rhe h
+    await newListing.save();
+    res.redirect("/listings");
+
+
+//let {title,description,image,price,country, location}=req.body;
+//let listing= req.body.listing;
+
+}))
+
+//Edit Route
+router.get("/:id/edit",wrapAsync(async(req,res)=>{
+let {id}=req.params;
+const  listing = await Listing.findById(id);
+ res.render("./listings/edit.ejs",{listing});
+}))
+
+//Update Route
+router.put("/:id",validateListing,wrapAsync(async(req,res)=>{
+
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});//js ki obj hai jiske ander sare parameters h recontruct krk unn parameter ko individual value me convert krenge jisko hum nayi updated value me pass krenge
+    res.redirect(`/listings/${id}`);
+}))
+
+//DELETE ROUTE
+router.delete("/:id",wrapAsync( async(req,res)=>{
+    let {id}=req.params;
+    let deletedListing=await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+}))
+
+module.exports=router;
