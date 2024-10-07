418cadfe0d90d5ddb6d1ae4f1187bf2ebf45d56b
Ishika Chourase <ishikachourase4@gmail.com>
2024-10-07 21:11:14 +0530

    Added flash error  msg include

--- routes/listing.js
+++ routes/listing.js
@@ -35,6 +35,10 @@ const validateListing=(req,res,next)=>{
  router.get ("/:id",wrapAsync(async(req,res)=>{
      let {id}=req.params;
     const  listing = await Listing.findById(id).populate("reviews");
+    if(!listing){
+        req.flash("error","Listing you requested for does not exist!");
+        res.redirect("/listings")
+    }
      res.render("./listings/show.ejs",{listing});
  }))
 
@@ -58,6 +62,10 @@ router.post("/",validateListing,wrapAsync (async(req,res,next)=>{
 router.get("/:id/edit",wrapAsync(async(req,res)=>{
 let {id}=req.params;
 const  listing = await Listing.findById(id);
+if(!listing){
+    req.flash("error","Listing you requested for does not exist!");
+    res.redirect("/listings")
+}
  res.render("./listings/edit.ejs",{listing});
 }))
 
