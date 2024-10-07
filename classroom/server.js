0dd45a3d0ccdff6f3045cdf649db68d0874b96b8
Ishika Chourase <ishikachourase4@gmail.com>
2024-10-07 16:53:21 +0530

    added connect-flash msgs

--- classroom/server.js
+++ classroom/server.js
@@ -3,19 +3,54 @@ const app= express();
 const users= require("./routes/user.js");
 const posts= require("./routes/post.js");
 const session=require("express-session");
+const flash=require("connect-flash");
+const path=require("path");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
 
-app.use(session({
+
+  const sessionOptions ={
     secret: 'mysupersecretstring', resave :false,saveUninitialized: true
-  }));
+  }
 
-  app.get("/reqcount",(req,res)=>{
-    if(req.session.count){
-        req.session.count++;
-    }else{
-        req.session.count=1;
-    }
-    res.send(`you sent a request ${req.session.count}times`);
+  app.use(session(sessionOptions));
+  app.use(flash());
+
+app.use((req,res,next)=>{
+    res.locals.successMsg=req.flash("success");
+    res.locals.errorMsg=req.flash("error");
+    next();
 })
+
+app.get("/register",(req,res)=>{
+    let {name="anonymous"}=req.query; //name ko extract krna hai
+   req.session.name=name;
+   if(name==="anonymous"){
+    req.flash("error","User not registered");
+   }
+   else{
+    req.flash("success","User Register Successfully");
+   }
+  
+   
+   res.redirect("/hello");
+});
+
+
+
+app.get("/hello",(req,res)=>{
+   
+res.render("page.ejs",{name :req.session.name });
+});
+
+//   app.get("/reqcount",(req,res)=>{
+//     if(req.session.count){
+//         req.session.count++;
+//     }else{
+//         req.session.count=1;
+//     }
+//     res.send(`you sent a request ${req.session.count}times`);
+// })
 
 // app.get("/test",(req,res)=>{
 //     res.send("test successful");
