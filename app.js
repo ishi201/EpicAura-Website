042d71671554d2e2ffac53dabdbed3c35af56abf
Ishika Chourase <ishikachourase4@gmail.com>
2024-10-09 13:22:43 +0530

    Created SignUp page-post

--- app.js
+++ app.js
@@ -77,14 +77,14 @@ app.use((req,res,next)=>{
 
 
 
-app.get("/demouser",async(req,res)=>{
-  let fakeUser = new User({
-    email :"student@gmail.com",
-    username :"delta-student"
-  });
- let registeredUser=await User.register(fakeUser,"helloworld");
- res.send(registeredUser);
-})
+// app.get("/demouser",async(req,res)=>{
+//   let fakeUser = new User({
+//     email :"student@gmail.com",
+//     username :"delta-student"
+//   });
+//  let registeredUser=await User.register(fakeUser,"helloworld");
+//  res.send(registeredUser);
+// })
 
 
 
