3c99f5894c81cd0921423119f7c87cf7a3018333
Ishika Chourase <ishikachourase4@gmail.com>
2024-10-15 18:20:37 +0530

    created MVC for listing

---
+++ controllers/listings.js
@@ -0,0 +1,64 @@
+const Listing=require("../models/listing");
+
+module.exports.index=async(req,res)=>{
+    const allListings= await Listing.find({});
+     res.render("listings/index.ejs",{allListings})
+     }
+
+module.exports.renderNewForm=(req,res)=>{
+    res.render("./listings/new.ejs");
+ }
+
+ module.exports.showListing=async(req,res)=>{
+    let {id}=req.params;
+   const  listing = await Listing.findById(id).populate({path :"reviews",
+        populate:{path:"author"},
+        }).populate("owner");
+   if(!listing){
+       req.flash("error","Listing you requested for does not exist!");
+       res.redirect("/listings")
+   }
+   console.log(listing);
+    res.render("./listings/show.ejs",{listing});
+}
+
+module.exports.createListing=async(req,res,next)=>{
+    
+    const newListing = new Listing (req.body.listing)  //instance create kr rhe h
+    newListing.owner=req.user._id;
+    await newListing.save();
+    req.flash("success","New Listing Created!");
+    res.redirect("/listings");
+
+
+//let {title,description,image,price,country, location}=req.body;
+//let listing= req.body.listing;
+
+}
+
+module.exports.editListing=async(req,res)=>{
+    let {id}=req.params;
+    const  listing = await Listing.findById(id);
+    if(!listing){
+        req.flash("error","Listing you requested for does not exist!");
+        res.redirect("/listings")
+    }
+     res.render("./listings/edit.ejs",{listing});
+     }
+
+module.exports.updateListing=async(req,res)=>{
+
+    let {id}=req.params;
+   
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});//js ki obj hai jiske ander sare parameters h recontruct krk unn parameter ko individual value me convert krenge jisko hum nayi updated value me pass krenge
+    req.flash("success","Listing Updated!");
+    res.redirect(`/listings/${id}`);
+}    
+
+module.exports.deleteListing=async(req,res)=>{
+    let {id}=req.params;
+    let deletedListing=await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    req.flash("success","Listing Deleted!");
+    res.redirect("/listings");
+}
