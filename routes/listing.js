9406249f271fb26f674b15ac3bd7546aa0793a93
Ishika Chourase <ishikachourase4@gmail.com>
2024-10-16 21:25:52 +0530

    created rating style and image upload feature

--- routes/listing.js
+++ routes/listing.js
@@ -4,12 +4,16 @@ const wrapAsync=require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const {isLoggedIn , isOwner , validateListing}=require("../middleware.js");
 const listingController = require("../controllers/listings.js");
-
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 //index and create route added
 router.route("/")
 .get(wrapAsync(listingController.index) )
-.post(validateListing,isLoggedIn,wrapAsync (listingController.createListing))
+//.post(validateListing,isLoggedIn,wrapAsync (listingController.createListing))
+.post( upload.single('listing[image]'),(req,res)=>{
+    res.send(req.file);
+})
 
  //New route
  router.get("/new",isLoggedIn,listingController.renderNewForm);
