77ac5231df02c2f0e449c0a5632751b084337f58
Ishika Chourase <ishikachourase4@gmail.com>
2024-10-22 20:23:43 +0530

    Storing Coordinates

--- controllers/listings.js
+++ controllers/listings.js
@@ -31,10 +31,8 @@ module.exports.createListing=async(req,res,next)=>{
     query :req.body.listing.location,
     limit: 1,
  })
- .send();
- console.log(response.body.features[0].geometry);
- res.send("done!");
 
+ .send();
 
     let url= req.file.path;
     let filename= req.file.filename;
@@ -42,7 +40,10 @@ module.exports.createListing=async(req,res,next)=>{
     const newListing = new Listing (req.body.listing) ; //instance create kr rhe h
     newListing.owner=req.user._id;
     newListing.image={url,filename};
-    await newListing.save();
+    newListing.geometry=response.body.features[0].geometry;
+
+    let savedListing=await newListing.save();
+    console.log(savedListing);
     req.flash("success","New Listing Created!");
     res.redirect("/listings");
 
